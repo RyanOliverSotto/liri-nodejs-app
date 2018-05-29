b3f122b7daaeba80b3eaacb5b5fc587f3aa4b04e
ryanoliversotto <ryan.sotto@outlook.com>
2018-05-29 11:46:25 -0500

    Resolved bugs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # liri-nodejs-app
 Northwestern Coding Bootcamp Spring 2018 Homework Assignment Week10: LIRI Bot
+
+Overview
+
+In this assignment, you will make LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-// Your personal keys stored here
 require("dotenv").config();
 
 // NPM - Packages && Required files
@@ -20,7 +19,6 @@ var search = process.argv[2];
 var entireCmdLine = process.argv;
 var findThis = "";
 
-
 // Text colors
 var FgBlue = "\x1b[34m";
 var FgWhite = "\x1b[0m";
@@ -29,6 +27,7 @@ var FgGreen = "\x1b[32m";
 var FgMagenta = "\x1b[35m";
 
 // Turns user search into string for placement in urls (OMDB & Spotify)
+// i === 3 to capture the first meaningful userinput
 for (var i = 3; i < entireCmdLine.length; i++) {
     if (i > 3 && i < entireCmdLine.length) {
         findThis = findThis + "+" + entireCmdLine[i];
@@ -38,27 +37,25 @@ for (var i = 3; i < entireCmdLine.length; i++) {
 };
 
 // Searches for results based on userinput 
-function liriBot () {
+function liriBot() {
 
-    switch(search) {
+    switch (search) {
         case "my-tweets":
-        
-            var params = {screen_name: 'CoderChicago'};
+            var params = { screen_name: 'CoderChicago' };
             var logIt1 = search.concat(findThis + ", ")
-            console.log ("Searching "+ FgCyan + "Twitter" + FgWhite + " for your tweets!\n");
-
-            client.get('statuses/user_timeline', params, function(error, tweets, response) {
+            console.log("Searching " + FgCyan + "Twitter" + FgWhite + " for your tweets!\n");
+            client.get('statuses/user_timeline', params, function (error, tweets, response) {
                 if (!error) {
                     console.log(FgCyan + "----------Twitter Last 20 Tweets!----------\n" + FgWhite);
-                    for (i = 0; i < tweets.length && i < 20; i ++) {
+                    for (i = 0; i < tweets.length && i < 20; i++) {
                         console.log(FgCyan + (i + 1) + ": " + FgWhite + tweets[i].text);
                         console.log(FgCyan + "-- " + tweets[i].created_at.slice(0, 20) + FgWhite);
-                        var logIt2 =logIt1 + tweets[i].created_at.slice(0, 20) + " " + tweets[i].text;
-                        fs.appendFile("log.txt", logIt2 + "\n", function(err) {
+                        var logIt2 = logIt1 + tweets[i].created_at.slice(0, 20) + " " + tweets[i].text;
+                        fs.appendFile("log.txt", logIt2 + "\n", function (err) {
                             if (err) {
-                              return console.log(err);
+                                return console.log(err);
                             }
-                           logIt2 = ""; 
+                            logIt2 = "";
                         });
 
                     };
@@ -73,14 +70,14 @@ function liriBot () {
 
             if (findThis === "") {
                 findThis = "The+sign+ace+of+base" //  If the user doesn't type a song in, the program will output data for the song "The Sign."
-                logIt +="The Sign Ace of Base";
+                logIt += "The Sign Ace of Base";
             }
-        
-            console.log ("Searching "+ FgGreen + "Spotify..." + FgWhite + "\n");
 
-            spotify.search({ type: 'track', query: findThis }, function(err, data) {
+            console.log("Searching " + FgGreen + "Spotify..." + FgWhite + "\n");
+
+            spotify.search({ type: 'track', query: findThis }, function (err, data) {
                 if (err) {
-                return console.log('Error occurred: ' + err);
+                    return console.log('Error occurred: ' + err);
                 }
 
                 console.log(FgGreen + "----------Spotify Search Results!----------\n" + FgWhite);
@@ -89,9 +86,9 @@ function liriBot () {
                 console.log(FgGreen + 'Album: ' + FgWhite + data.tracks.items[0].album.name);
                 console.log(terminalLink(FgGreen + 'Preview:' + FgWhite, data.tracks.items[0].artists[0].external_urls.spotify));
                 console.log(FgGreen + "\n-------------------------------------------" + FgWhite);
-                fs.appendFile("log.txt", logIt + "\n", function(err) {
+                fs.appendFile("log.txt", logIt + "\n", function (err) {
                     if (err) {
-                      return console.log(err);
+                        return console.log(err);
                     }
                     console.log("\nlog.txt was updated!\n");
                 });
@@ -107,10 +104,10 @@ function liriBot () {
             var queryUrl = "http://www.omdbapi.com/?t=" + findThis + "&y=&plot=short&apikey=" + keys.omdb.key;
             var logIt = search.concat("," + findThis)
 
-            console.log ("Searching "+ FgBlue + "OMDB..." + FgWhite + "\n") 
+            console.log("Searching " + FgBlue + "OMDB..." + FgWhite + "\n")
 
-            request(queryUrl, function(error, response, body) {
-                
+            request(queryUrl, function (error, response, body) {
+
                 if (error) {
                     console.log(error);
                 } else if (!error && response.statusCode === 200 && JSON.parse(body).Ratings !== undefined) {
@@ -124,9 +121,9 @@ function liriBot () {
                     console.log(FgBlue + "Rotten Tomatoes Rating: " + FgWhite + JSON.parse(body).Ratings[1].Value);
                     console.log(FgBlue + "Plot: " + FgWhite + JSON.parse(body).Plot);
                     console.log(FgBlue + "\n---------------------------------------------" + FgWhite);
-                    fs.appendFile("log.txt", logIt +"\n", function(err) {
+                    fs.appendFile("log.txt", logIt + "\n", function (err) {
                         if (err) {
-                          return console.log(err);
+                            return console.log(err);
                         }
                         console.log("\nlog.txt was updated!\n");
                     });
@@ -139,31 +136,17 @@ function liriBot () {
 
         case "do-what-it-says":
 
-            fs.readFile("random.txt", "utf8", function(error, data) {
-
+            fs.readFile("random.txt", "utf8", function (error, data) {
+                // Break the string down by comma separation and store the contents into the output array.
                 var dataArr = data.split(",");
-                var randomPull = Math.floor(Math.random() * (dataArr.length - 1));
-                console.log("You randomly pulled with number: " + randomPull + "\n");
-   
                 if (error) {
                     return console.log(error);
                 }
-
-                if (randomPull % 2 !== 0) {
-                    search = dataArr[randomPull];
-                    findThis = dataArr[randomPull + 1];
-                } else if (randomPull % 2 === 0 && randomPull !== 0) {
-                    search = dataArr[randomPull - 1];
-                    findThis = dataArr[randomPull];
-                } else {
-                    search = dataArr[randomPull];
-                }
-                
-                console.log("Your random search has begun.\n")
-        
+                search = dataArr[0];
+                findThis = dataArr[1];
                 liriBot();
             });
-            
+
             break;
 
         default:
